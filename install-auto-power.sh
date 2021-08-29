5ad9085da0798a16f7eaf2b3b816050dd1e284ae
NamPT <ptn611@gmail.com>
2021-08-29 16:11:49 +0700

    Add files via upload

---
+++ install-auto-power.sh
@@ -0,0 +1,18 @@
+﻿## w8 LA < 1 to start by cmd: sudo bash /hive-config/install-auto-power.sh
+sudo mkdir /auto-power/ # 
+sudo chmod 777 /auto-power # 
+sudo mkdir /auto-power/logs/ # 
+sudo chmod 777 /auto-power/logs # 
+sudo apt-get update # 
+sudo apt-get install -y samba --install-recommends # 
+sudo apt-get update # 
+sudo apt-get install -y wget apt-transport-https # 
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb # 
+sudo dpkg -i packages-microsoft-prod.deb # 
+rm packages-microsoft-prod.deb # 
+sudo apt-get update # 
+#sudo apt-get install -y dotnet-runtime-5.0 # 
+sudo apt-get install -y dotnet-runtime-3.1 #
+sudo apt-get update # 
+sudo net usershare add auto-power /auto-power "auto-power" everyone:F guest_ok=y # 
+# 
