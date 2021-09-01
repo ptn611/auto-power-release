36fd1b13e96804a4383c239587cb0dd55ee5a611
NamPT <ptn611@gmail.com>
2021-09-01 09:01:42 +0700

    Update install-auto-power.sh

--- install-auto-power.sh
+++ install-auto-power.sh
@@ -1,17 +1,2 @@
-﻿# w8 LA < 1 & turn watchdog off to start by cmd: sudo bash /hive-config/install-auto-power.sh
-sudo mkdir /auto-power/ # 
-sudo chmod 777 /auto-power # 
-sudo mkdir /auto-power/logs/ # 
-sudo chmod 777 /auto-power/logs # 
-sudo apt-get update # 
-sudo apt-get install -y samba --install-recommends # 
-sudo apt-get update # 
-sudo apt-get install -y wget apt-transport-https # 
-wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb # 
-sudo dpkg -i packages-microsoft-prod.deb # 
-rm packages-microsoft-prod.deb # 
-sudo apt-get update # 
-sudo apt-get install -y dotnet-runtime-3.1 #
-sudo apt-get update # 
-sudo net usershare add auto-power /auto-power "auto-power" everyone:F guest_ok=y # 
-# 
+# Copy line below and run in terminal to install auto-power: 
+sudo wget -c https://github.com/ptn611/auto-power-release/releases/download/v1.0/install-auto-power.sh -O install-auto-power.sh && sudo bash install-auto-power.sh #
