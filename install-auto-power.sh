790553ece20270aae5edba09ca0b0b0f222989f1
NamPT <ptn611@gmail.com>
2021-09-04 17:27:38 +0700

    Create install-auto-power.sh

--- install-auto-power.sh
+++ install-auto-power.sh
@@ -1,2 +1,5 @@
 # Copy line below and run in terminal to install auto-power: 
+for Ubuntu 18.04:
 sudo wget -c https://github.com/ptn611/auto-power-release/releases/download/v1.0/install-auto-power.sh -O install-auto-power.sh && sudo bash install-auto-power.sh #
+for Ubuntu 20.04:
+sudo wget -c https://github.com/ptn611/auto-power-release/releases/download/v1.0/install-auto-power-v.20.04.sh -O install-auto-power-v.20.04.sh && sudo bash install-auto-power-v.20.04.sh #
